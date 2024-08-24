0d54f98395061976904960d7a1a3d562afc8c76b
Helen-MLT <h.milletti@gmail.com>
2024-08-24 12:52:21 +0200

    Added basic form handling + typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Custom Coffee Recipe",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+  let coffeeRecipeElement = document.querySelector("#coffee-recipe-generator");
+  coffeeRecipeElement.addEventListener("submit", generateRecipe);
+}
